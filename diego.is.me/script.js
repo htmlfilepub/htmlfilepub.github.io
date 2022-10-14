7f973377d9ad712ebda99d8a9987b889d9b0419e
htmlfilepub <93841886+htmlfilepub@users.noreply.github.com>
2022-10-14 21:46:13 +0200

    Add files via upload

---
+++ diego.is.me/script.js
@@ -0,0 +1,25 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show")
+        }
+    });
+});
+const hidden = document.querySelectorAll('.hidden');
+hidden.forEach((el) => observer.observe(el));
+
+const observers = new IntersectionObserver((entrie) => {
+    entrie.forEach((entrys) => {
+        console.log(entrys)
+        if (entrys.isIntersecting) {
+            entrys.target.classList.add("show2");
+        } else {
+            entrys.target.classList.remove("show2")
+        }
+    });
+});
+const hiddens = document.querySelectorAll('.hidden2');
+hiddens.forEach((el) => observers.observe(el));
