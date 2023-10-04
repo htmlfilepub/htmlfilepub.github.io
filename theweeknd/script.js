e9faec68fadd25d9ccd317cecb5611f1ed8672a8
htmlfilepub <93841886+htmlfilepub@users.noreply.github.com>
2023-10-04 23:39:21 +0200

    Add files via upload

--- theweeknd/script.js
+++ theweeknd/script.js
@@ -5,3 +5,35 @@ toggleButton.addEventListener('click', () => {
     listaMobile.classList.toggle('active')
     toggleButton.classList.toggle('active')
   })
+
+  var n_img = 3;
+  corrente = 1;
+
+  function next() {
+    corrente++; 
+
+    if (corrente>n_img) {
+      corrente = 1
+    }
+
+    for (i = n_img; i>0; i--) {
+      document.querySelector('#img_slider img:nth-child(' + i + ')').style.display = "none";
+    }
+
+    document.querySelector('#img_slider img:nth-child(' + corrente + ')').style.display = "block";
+  }
+
+
+  function before() {
+    corrente--; 
+
+    if (corrente == 0) {
+      corrente = n_img;
+    }
+
+    for (i = n_img; i>0; i--) {
+      document.querySelector('#img_slider img:nth-child(' + i + ')').style.display = "none";
+    }
+
+    document.querySelector('#img_slider img:nth-child(' + corrente + ')').style.display = "block";
+  }
