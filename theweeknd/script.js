a3b89b2c5230c0969e1429e61b517d423caa4a10
htmlfilepub <93841886+htmlfilepub@users.noreply.github.com>
2023-10-04 22:02:17 +0200

    Add files via upload

--- theweeknd/script.js
+++ theweeknd/script.js
@@ -3,4 +3,5 @@ const listaMobile = document.getElementsByClassName('lista_mobile')[0]
 
 toggleButton.addEventListener('click', () => {
     listaMobile.classList.toggle('active')
+    toggleButton.classList.toggle('active')
   })
