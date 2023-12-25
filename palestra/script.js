3278de182beb0b5e3a4263652568420d6105472d
htmlfilepub <93841886+htmlfilepub@users.noreply.github.com>
2023-12-25 12:00:00 +0100

    Add files via upload

---
+++ palestra/script.js
@@ -0,0 +1,51 @@
+var n_img = 10;
+corrente = 1;
+
+function next() {
+  corrente++; 
+
+  if (corrente>n_img) {
+    corrente = 1
+  }
+
+  for (i = n_img; i>0; i--) {
+    document.querySelector('#img_slider img:nth-child(' + i + ')').style.display = "none";
+    document.querySelector('#es_titles h1:nth-child(' + i + ')').style.display = "none";
+    document.querySelector('#es_rec p:nth-child(' + i + ')').style.display = "none";
+    document.querySelector('#es_rip p:nth-child(' + i + ')').style.display = "none";
+
+
+  }
+
+  document.querySelector('#img_slider img:nth-child(' + corrente + ')').style.display = "block";
+  document.querySelector('#es_titles h1:nth-child(' + corrente + ')').style.display = "block";
+  document.querySelector('#es_rec p:nth-child(' + corrente + ')').style.display = "block";
+  document.querySelector('#es_rip p:nth-child(' + corrente + ')').style.display = "block";
+
+
+}
+
+
+function before() {
+  corrente--; 
+
+  if (corrente == 0) {
+    corrente = n_img;
+  }
+
+  for (i = n_img; i>0; i--) {
+    document.querySelector('#img_slider img:nth-child(' + i + ')').style.display = "none";
+    document.querySelector('#es_titles h1:nth-child(' + i + ')').style.display = "none";
+    document.querySelector('#es_rec p:nth-child(' + i + ')').style.display = "none";
+    document.querySelector('#es_rip p:nth-child(' + i + ')').style.display = "none";
+
+
+  }
+
+  document.querySelector('#img_slider img:nth-child(' + corrente + ')').style.display = "block";
+  document.querySelector('#es_titles h1:nth-child(' + corrente + ')').style.display = "block";
+  document.querySelector('#es_rec p:nth-child(' + corrente + ')').style.display = "block";
+  document.querySelector('#es_rip p:nth-child(' + corrente + ')').style.display = "block";
+
+
+}
